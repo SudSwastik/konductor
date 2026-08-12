44a6fca1bad3279703a11c488133d65703466815
SudSwastik <sudarshan4309@gmail.com>
2026-08-13 00:31:55 +0530

    Create projector Spring Boot scaffold

---
+++ projector/src/main/java/com/konductor/projector/ProjectorApplication.java
@@ -0,0 +1,12 @@
+package com.konductor.projector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectorApplication.class, args);
+    }
+}
